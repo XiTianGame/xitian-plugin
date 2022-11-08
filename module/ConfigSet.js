7c158f5a0ad7259cc97cb00b7141890d966bf75b
XiTianGame <99065478+XiTianGame@users.noreply.github.com>
2022-11-08 19:29:14 +0800

    非人工变更设置保留注释

--- module/ConfigSet.js
+++ module/ConfigSet.js
@@ -14,6 +14,9 @@ class ConfigSet {
         this.configPath = "./plugins/xitian-plugin/config/";
         this.config = {};
 
+        /** 文件读取 */
+        this.file = { config: {}, defSet: {} };
+
         /** 监听文件 */
         this.watcher = { config: {}, defSet: {} };
     }
@@ -41,20 +44,37 @@ class ConfigSet {
     }
 
     /**
+     * 读取文件
+     * @param app 功能
+     * @param name 名称
+     * @param type 默认跑配置-defSet，用户配置-config
+     */
+    getFile(app, name, type = "defSet"){
+        let file = this.getFilePath(app, name, type);
+        let key = `${app}.${name}`;
+
+        if(this.file[type][key]) return this.file[type][key];
+
+        this.file[type][key] = fs.readFileSync(file, "utf8");
+
+        this.watch(file, app, name, type);
+
+        return this.file[type][key]
+    }
+
+    /**
      * 获取配置yaml
      * @param app 功能
      * @param name 名称
      * @param type 默认跑配置-defSet，用户配置-config
      */
-    getYaml(app, name, type) {
-        let file = this.getFilePath(app, name, type);
+     getYaml(app, name, type = "defSet") {
         let key = `${app}.${name}`;
 
         if (this[type][key]) return this[type][key];
 
-        this[type][key] = YAML.parse(fs.readFileSync(file, "utf8"));
-
-        this.watch(file, app, name, type);
+        if (!this.file[type][key]) this.getFile(app, name, type);
+        this[type][key] = YAML.parse(this.file[type][key]);
 
         return this[type][key];
     }
@@ -73,6 +93,7 @@ class ConfigSet {
         const watcher = chokidar.watch(file);
         watcher.on("change", (path) => {
             delete this[type][key];
+            delete this.file[type][key];
             logger.mark(`[修改配置文件][${type}][${app}][${name}]`);
             if (this[`change_${app}${name}`]) {
                 this[`change_${app}${name}`]();
@@ -82,14 +103,65 @@ class ConfigSet {
         this.watcher[type][key] = watcher;
     }
 
+    /**
+     * 保存设置
+     * @param app 功能
+     * @param name 名称
+     * @param type 类型
+     * @param data 数据
+     */
     saveSet(app, name, type, data) {
         let file = this.getFilePath(app, name, type);
+        let origin = this.getFile(app, name, type);
+        let notes = this.getNote(origin)
         if (lodash.isEmpty(data)) {
             fs.existsSync(file) && fs.unlinkSync(file);
         } else {
-            let yaml = YAML.stringify(data);
-            fs.writeFileSync(file, yaml, "utf8");
+            data = YAML.stringify(data);
+            //插入注释
+            data = this.Insert(notes, data);
+            
+            fs.writeFileSync(file, data, "utf8");
         }
+    }
+
+    /**获取yaml注释 */
+    getNote(file){
+        let annotate = {}
+        file = file.split('\n')
+        let tmp = [];
+        for(let v of file){
+            if(v.startsWith('#')){
+                tmp.push(v);
+            }else{
+                v = v.split(":");
+                if(v.length<2){
+                    continue;
+                }
+                annotate[v[0]] = tmp;
+                tmp = [];
+            }
+        }   
+        return annotate;
+    }
+
+    /**
+     * 填充注释
+     * @param notes 注释本体
+     * @param data 数据
+     */
+    Insert(notes, data){
+        data = data.split('\n')
+        for(let key of Object.keys(notes)){
+            for(let i = 0;i<data.length;i++){
+                if(data[i].startsWith(`${key}:`)){
+                    data.splice(i,0,notes[key].join(""))
+                    i++
+                }
+            };
+        }
+        data = data.join('\n');
+        return data
     }
 }
 
