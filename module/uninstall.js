73a484e8eed012b1fff15be4dca2bf812606ee8b
XiTianGame <99065478+XiTianGame@users.noreply.github.com>
2024-08-26 21:56:35 +0800

    💥清除多余代码

--- module/uninstall.js
+++ module/uninstall.js
@@ -1,8 +1,6 @@
 import { Restart } from '../../other/restart.js'
 import fs from 'node:fs'
 
-const _path = process.cwd();
-
 class uninstall {
   /**
    * 删除插件
@@ -10,7 +8,7 @@ class uninstall {
    */
   async removePlugin(e) {
     let name = e.msg.replace(/#|彻底|删除插件/g, "");
-    let path = `${_path}/plugins/${name}`;
+    let path = `./plugins/${name}`;
     if (!fs.existsSync(path)) return false;
     //包含git文件夹
     if (!fs.statSync(`${path}/.git`).isDirectory()) return false;
